cb6a15cb88fae119a514711442be6f252f13c380
A. Legayada <tonilegayada@gmail.com>
2025-10-02 00:38:30 +0800

    array opimizaion

--- tf_demo_2/tf_demo_2.ino
+++ tf_demo_2/tf_demo_2.ino
@@ -3,36 +3,54 @@
 #define RED_LED_PIN 12
 #define GREEN_LED_PIN 11
 #define YELLOW_LED_PIN 10
+#define LED_PIN_ARRAY_SIZE 3
 
-int LED_BLINK_STATE = 1;
+int LEDBlinkState = 1;
+// type of pins is actually a byte
+byte LEDPinArray[LED_PIN_ARRAY_SIZE] = 
+  {RED_LED_PIN, GREEN_LED_PIN, YELLOW_LED_PIN}; 
 
-
-void setup() {
-  pinMode(RED_LED_PIN, OUTPUT);
-  pinMode(GREEN_LED_PIN, OUTPUT);
-  pinMode(YELLOW_LED_PIN, OUTPUT);
-
-  Serial.begin(9600);
-  digitalWrite(RED_LED_PIN, LOW);
-  digitalWrite(GREEN_LED_PIN, LOW);
-  digitalWrite(YELLOW_LED_PIN, LOW);
+void setLEDPinMode(){
+  for (int i = 0 ; i < LED_PIN_ARRAY_SIZE; i++){
+    pinMode(LEDPinArray[i], OUTPUT);
+  }
 }
 
-void loop() {
-  if (digitalRead(BTN_PIN) == LOW) {
-    if (LED_BLINK_STATE == 1){
+void turnOffAllLEDS(){
+  for (int i = 0; i < LED_PIN_ARRAY_SIZE; i++){
+    digitalWrite(LEDPinArray[i], LOW);
+  }
+}
+
+void toggleLEDS(){
+
+  if (LEDBlinkState == 1){
       digitalWrite(RED_LED_PIN, HIGH);
       digitalWrite(GREEN_LED_PIN, LOW);
       digitalWrite(YELLOW_LED_PIN, HIGH);
-      LED_BLINK_STATE = 2;
+      LEDBlinkState = 2;
 
     } else{
       digitalWrite(RED_LED_PIN, LOW);
       digitalWrite(GREEN_LED_PIN, HIGH);
       digitalWrite(YELLOW_LED_PIN, LOW);
-      LED_BLINK_STATE = 1;
+      LEDBlinkState = 1;
     }
 
-      delay(300);
+      
+
+
+}
+void setup() {
+  setLEDPinMode();
+  turnOffAllLEDS();
+  Serial.begin(9600);
+ 
+}
+
+void loop() {
+  if (digitalRead(BTN_PIN) == LOW) {
+    toggleLEDS();
+    delay(300);
   }
 }

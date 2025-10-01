e709d5e6edaef66b9cf5e853d5eeb4c61b275066
A. Legayada <tonilegayada@gmail.com>
2025-10-01 23:58:21 +0800

    better sequencing and handlig of states demo

--- tf_demo_2/tf_demo_2.ino
+++ tf_demo_2/tf_demo_2.ino
@@ -4,31 +4,35 @@
 #define GREEN_LED_PIN 11
 #define YELLOW_LED_PIN 10
 
+int LED_BLINK_STATE = 1;
 
 
-void setup() { 
-pinMode(RED_LED_PIN, OUTPUT);
-pinMode(GREEN_LED_PIN, OUTPUT);
-pinMode(YELLOW_LED_PIN, OUTPUT);
+void setup() {
+  pinMode(RED_LED_PIN, OUTPUT);
+  pinMode(GREEN_LED_PIN, OUTPUT);
+  pinMode(YELLOW_LED_PIN, OUTPUT);
 
-Serial.begin(9600);
-digitalWrite(RED_LED_PIN, LOW);
-digitalWrite(GREEN_LED_PIN, LOW);
-digitalWrite(YELLOW_LED_PIN, LOW);
+  Serial.begin(9600);
+  digitalWrite(RED_LED_PIN, LOW);
+  digitalWrite(GREEN_LED_PIN, LOW);
+  digitalWrite(YELLOW_LED_PIN, LOW);
 }
 
 void loop() {
- if (digitalRead(BTN_PIN) == HIGH){
-  Serial.println("Pressed");
- } else{
-  digitalWrite(RED_LED_PIN, HIGH);
-  digitalWrite(GREEN_LED_PIN, LOW);
-  digitalWrite(YELLOW_LED_PIN, HIGH);
-  delay(300);
-   digitalWrite(RED_LED_PIN, LOW);
-  digitalWrite(GREEN_LED_PIN, HIGH);
-  digitalWrite(YELLOW_LED_PIN, LOW);
-   delay(300);
- }
+  if (digitalRead(BTN_PIN) == LOW) {
+    if (LED_BLINK_STATE == 1){
+      digitalWrite(RED_LED_PIN, HIGH);
+      digitalWrite(GREEN_LED_PIN, LOW);
+      digitalWrite(YELLOW_LED_PIN, HIGH);
+      LED_BLINK_STATE = 2;
 
+    } else{
+      digitalWrite(RED_LED_PIN, LOW);
+      digitalWrite(GREEN_LED_PIN, HIGH);
+      digitalWrite(YELLOW_LED_PIN, LOW);
+      LED_BLINK_STATE = 1;
+    }
+
+      delay(300);
+  }
 }

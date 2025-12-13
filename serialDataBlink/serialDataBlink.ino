864d3251b7ff47f7a6126c3f99111e8baa33a88b
A. Legayada <tonilegayada@gmail.com>
2025-12-13 13:45:34 +0800

    Add serialDataBlink Arduino sketch
    Initial commit of serialDataBlink.ino, which blinks a red LED on pin 12 for a duration received via serial input. The sketch sets up serial communication and listens for integer values to control the blink duration.

---
+++ serialDataBlink/serialDataBlink.ino
@@ -0,0 +1,33 @@
+// #define POTENTIONMETER_PIN A2
+// #define BTN_PIN 9
+#define RED_LED_PIN 12
+// #define GREEN_LED_PIN 11
+// #define YELLOW_LED_PIN 10
+// #define LED_PIN_ARRAY_SIZE 3
+
+
+void setup() {
+
+  pinMode(RED_LED_PIN,OUTPUT);
+  digitalWrite(RED_LED_PIN, LOW);
+  Serial.begin(9600);
+
+  Serial.setTimeout(100);
+  delay(100);
+ 
+}
+
+void blink(int value){
+  digitalWrite(RED_LED_PIN, HIGH);
+  delay(value);
+   digitalWrite(RED_LED_PIN, LOW);
+}
+
+void loop() {
+
+  if(Serial.availableForWrite() > 0){
+      int pause = Serial.parseInt();
+       blink(pause);
+    }
+  
+}

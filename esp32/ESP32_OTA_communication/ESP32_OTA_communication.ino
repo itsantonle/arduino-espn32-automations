e0076d8448327f495fa6fbfdbfb4e701ea6706b5
A. Legayada <tonilegayada@gmail.com>
2026-01-08 00:57:54 +0800

    Add ESP32 OTA communication example
    Introduces a new ESP32_OTA_communication.ino file demonstrating WiFi setup, OTA updates, and WiFiManager integration for over-the-air programming of ESP32 devices.

---
+++ esp32/ESP32_OTA_communication/ESP32_OTA_communication.ino
@@ -0,0 +1,34 @@
+#include <WiFi.h>
+#include <ESPmDNS.h> 
+//UDP allow OTA(over the air) happenings
+#include <WiFiUDP.h> 
+// allow arduino IDE to connect to ESP OTA (no USB) 
+// alwyas include OTA code otherwise connection is severed
+#include <ArduinoOTA.h>
+#include <WiFiManager.h>
+
+const char *ssid = "esp32AP";
+const char *password = "password";
+
+void setup() {
+  // put your setup code here, to run once:
+  WiFi.mode(WIFI_STA);
+  Serial.begin(115200);
+  WiFiManager wm;
+  bool response; 
+  response = wm.autoConnect(ssid, password);
+  if(!response){
+    Serial.println("Not Connected")
+  }else{
+    Serial.println("Connect to wifi")
+  }
+  ArduinoOTA.setPassword("admin");
+  ArduinoOTA.begin();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  ArduinoOTA.handle();
+
+}
